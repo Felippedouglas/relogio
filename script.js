906178c29e89c83ac29b8346b9ff26f3e9634962
Felippe Douglas <dfelipex18@gmail.com>
2022-07-08 21:36:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,96 +1,37 @@
 function startTime() {
-    var hoje = new Date();
-    var horas = hoje.getHours();
-    var minutos = hoje.getMinutes();
-    var segundos = hoje.getSeconds();
-    var dias = hoje.getDay();
-    var data = hoje.getDate();
-    var mes = hoje.getMonth();
-    var ano = hoje.getFullYear();
+    var dataHoje = new Date();
+    var horas = String(dataHoje.getHours()).padStart(2, '0');
+    var minutos = String(dataHoje.getMinutes()).padStart(2, '0');
+    var segundos = String(dataHoje.getSeconds()).padStart(2, '0');
+    var dias = dataHoje.getDay();
+    var data = String(dataHoje.getDate()).padStart(2, '0');
+    var mes = dataHoje.getMonth();
+    var ano = dataHoje.getFullYear();
     var dia = dias;
 
-    switch (dia) {
-        case 0:
-            dia = "domingo";
-            break;
-        case 1:
-            dia = "segunda";
-            break;
-        case 2:
-            dia = "terça-feira";
-            break;
-        case 3:
-            dia = "quarta-feira";
-            break;
-        case 4:
-            dia = "quinta-feira";
-            break;
-        case 5:
-            dia = "sexta-feira";
-            break;
-        case 6:
-            dia = "sabado";
-            break;
-    }
-
-    switch (mes) {
-        case 0:
-            mes = "janeiro";
-            break;
-        case 1:
-            mes = "fevereiro";
-            break;
-        case 2:
-            mes = "março";
-            break;
-        case 3:
-            mes = "abril";
-            break;
-        case 4:
-            mes = "maio";
-            break;
-        case 5:
-            mes = "junho";
-            break;
-        case 6:
-            mes = "julho";
-            break;
-        case 7:
-            mes = "agosto";
-            break;
-        case 8:
-            mes = "setembro";
-            break;
-        case 9:
-            mes = "outubro";
-            break;
-        case 10:
-            mes = "novembro";
-            break;
-        case 11:
-            mes = "dezembro";
-            break;
-    }
     
-    // adicione um zero na frente de números<10
-    minutos = checkTime(minutos);
-    segundos = checkTime(segundos);
-    document.getElementById('txt').innerHTML = dia + "," + " " + data + " " + "de" + " " + mes + " " + "de" + " " + ano + " " + " | " + horas + ":" + minutos + ":" + segundos;
-    document.getElementById('hora').innerHTML = horas + ":" + minutos;
-    t = setTimeout('startTime()', 500);    
+    // arrays //
+    var diasSemana = ['domingo', 'segunda', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sabado']
+    var meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
 
-    if (ano < 2022) {
-        document.getElementById('txt').innerHTML = "verifique a data do seu dispositivo!" + "<br>" + dia + "," + " " + data + " " + "de" + " " + mes + " " + "de" + " " + ano + " " + " | " + horas + ":" + minutos + ":" + segundos + " " + " " + " " + "<br>" + "o ano: " + "''" + ano + "''" + " Está errado ";
+    if (horas >= 6 && horas <= 11) {
+        document.getElementById('mensagem').innerHTML = 'Bom dia :)'
+        document.body.style.backgroundImage = 'url(./imagens/manha.jpg)'
+    } else if (horas >= 12 && horas <= 17) {
+        document.getElementById('mensagem').innerHTML = 'Boa tarde :)'
+        document.body.style.backgroundImage = 'url(https://www.portalveneza.com.br/wp-content/uploads/2019/06/tempo-clima-sol-nuvens.jpg)'
+    } else if ((horas >= 18 && horas <= 23) || (horas >= 0 && horas <= 5)) {
+        document.getElementById('mensagem').innerHTML = 'Boa noite :)'
+        document.body.style.backgroundImage = 'url(./imagens/noite.jpg)'
+    }
+
+    var txtData = document.getElementById('txt-data')
+    txtData.innerHTML = `${diasSemana[dia]}, ${data} de ${meses[mes]} de ${ano} | ${horas}:${minutos}:${segundos}`;
+    document.getElementById('hora').innerHTML = `${horas}:${minutos}`;
+
+    if (ano != 2022) {
+        txtData.innerHTML = `verifique a data do seu dispositivo! <br> ${diasSemana[dia]}, ${data} de ${meses[mes]} de ${ano} | ${horas}:${minutos}:${segundos} <br> o ano: '' ${ano} '' Está errado`;
     }
 }
 
-function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
-}
-
-
-
-var tempo = setInterval(startTime,1000)
+setInterval( startTime, 500 )
